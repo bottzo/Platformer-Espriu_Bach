8efefa77bedc62d1988a113d54108f27ecac28ad
bottzo <45202057+bottzo@users.noreply.github.com>
2019-10-14 01:01:22 +0200

    creant el module player
    Cal crear ara una struct animation i guardarli les dades del tilset traientles del xml file el qual ia li e posat el xml_node player_node

--- Dev04_Handout/Motor2D/j1Player.h
+++ Dev04_Handout/Motor2D/j1Player.h
@@ -4,6 +4,16 @@
 #include "p2List.h"
 
 class j1Player :public j1Module {
+public:
+	j1Player();
+	~j1Player();
+	bool Awake(pugi::xml_node&config);
+	bool Load(const char* file_name);
+	bool CleanUp();
 
+private:
+	pugi::xml_document player_doc;
+	p2SString folder;
+	bool player_loaded;
 };
 #endif // __j1RENDER_H__
