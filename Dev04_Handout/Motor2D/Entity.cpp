792857d6d9a9ca984ba138a726c1beb0fcb8a24c
bottzo <45202057+bottzo@users.noreply.github.com>
2019-11-27 22:38:19 +0100

    juntat tot en el modul entities

--- Dev04_Handout/Motor2D/Entity.cpp
+++ Dev04_Handout/Motor2D/Entity.cpp
@@ -1,31 +1,31 @@
 #include "Entity.h"
 
-/*Entity* entity_manager::CreateEntity(Entity::Types type) {
+Entity* Entity::CreateEntity(Entity::Types type) {
 	//static_assert(Entity::Types::unknown == 2, "code needs update");
 	Entity* ret = nullptr;
-	switch (type) {
+	/*switch (type) {
 	case Entity::Types::enemy: ret = new enemy(); break;
 	case Entity::Types::player: ret = new player(); break;
-	}	if (ret != nullptr)
-		Entities.PushBack(ret);
+	}*/	if (ret != nullptr)
+		Entities.add(ret);
 	return ret;
 }
 
-void entity_manager::DestroyEntity(Entity* entity) {
+void Entity::DestroyEntity(Entity* entity) {
 
 }
 
-bool entity_manager::Update(float dt)
+bool Entity::Update(float dt)
 {
-	accumulated_time += dt;
+	/*accumulated_time += dt;
 	if (accumulated_time >= update_ms_cycle)
 		do_logic = true;
 	UpdateAll(dt, do_logic);
 	if (do_logic == true) {
 		accumulated_time = 0.0f;
 		do_logic = false;
-	}
+	}*/
 	return true;
 }
 
-Entity::Entity(Types type) : type(type) {}*/
+Entity::Entity(Types type) : type(type) {}
--- Dev04_Handout/Motor2D/Entity.h
+++ Dev04_Handout/Motor2D/Entity.h
@@ -1,12 +1,12 @@
 #ifndef __ENTITY_H__
 #define __ENTITY_H__
 #include "j1Module.h"
-#include "p2DynArray.h"
+#include "p2List.h"
 #include "p2Point.h"
 
 class SDL_Texture;
 
-/*class Entity :public j1Module{
+class Entity :public j1Module{
 public:
 	enum class Types
 	{
@@ -15,20 +15,18 @@ public:
 		unknown
 	};
 public:
+	bool Update(float dt);
+	bool UpdateAll(float dt, bool do_logic);
+
 	Entity(Types type);
-	virtual ~Entity();
-private:
+	//virtual ~Entity();
+	Entity* CreateEntity(Entity::Types type);
+	void DestroyEntity(Entity* entity);
+protected:
 	fPoint position;
 	Entity::Types type;
 	SDL_Texture* sprite;
+private:
+	p2List<Entity*> Entities;
 };
-
-struct entity_manager {
-	p2DynArray<Entity*> Entities;
-	Entity* CreateEntity(Entity::Types type);
-	void DestroyEntity(Entity* entity);
-	bool Update(float dt);
-	bool UpdateAll(float dt, bool do_logic);
-};*/
-
 #endif // __ENTITY_H__
